bcc913a5721eaa20867196b3f16c36107df3502c
239x1a32b7 <239x1a32b7@gprec.ac.in>
2025-09-02 16:58:34 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,16 @@
+package day2;
+
+public class Example {
+	static void HelloWorld()
+	{
+		System.out.println("HelloWorld");
+
+	}
+	public static void main(String[]args) {
+		HelloWorld();
+		HelloWorld();
+		HelloWorld();
+	
+	}
+
+}
