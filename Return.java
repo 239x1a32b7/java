5035014cc0ca449ca4164428ec72defd6a24c912
239x1a32b7 <239x1a32b7@gprec.ac.in>
2025-09-02 17:27:44 +0530

    Add files via upload

---
+++ Return.java
@@ -0,0 +1,16 @@
+package day2;
+
+public class Return {
+	static String HelloWorld()
+	{
+		System.out.println("Hello World");
+		return"";
+	}
+	public static void main(String[]args)
+	{
+		HelloWorld();
+		HelloWorld();
+		HelloWorld();
+
+	}
+}
