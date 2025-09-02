1ff37ebc87eb535c7cfd57258beeff08dc2e2127
239x1a32b7 <239x1a32b7@gprec.ac.in>
2025-09-02 16:31:34 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,13 @@
+package day2;
+public class demo {
+	public static void main(String[]args) {
+		double d=123.1;
+		int i=99;
+		System.out.println("i="+i);
+		System.out.println("d="+d);
+		System.out.println("After type conversion");
+		i=(int)d;
+		System.out.println("i="+i);
+		System.out.println("d="+d);
+	}
+}
